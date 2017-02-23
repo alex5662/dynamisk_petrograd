d6a4d270eb3bba3971f453f0c3e1116ab5775257
Alexander Jensen <Alexander Jensen>
2017-02-23 11:50:27 +0100

    load JSON produktliste and create template for each

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
+
+    // læs prduktliste
+
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
     visProdukt();
 
-    function visProdukt() {
-        // klone produkt_templaten
-        var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+}
 
-        // indsæt data i klon
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
 
 
 
-        // append klon til .produktliste
-        document.querySelector(".produktliste").appendChild(klon);
-    }
+function visProdukt(produkt) {
+    console.log(produkt);
+    // klone produkt_templaten
+    var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+
+    // indsæt data i klon
+
+
+
+    // append klon til .produktliste
+    document.querySelector(".produktliste").appendChild(klon);
 }
 
 
