8d3f6619907bb4b46d512e08f7356a6ef270dacd
Alexander Jensen <Alexander Jensen>
2017-02-23 11:39:14 +0100

    add javascript_template

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-klon.querySelector(".data_billede").src = "imgd/small/" + produkt.billede + "-sm.jpg";
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("siden vises");
+    visProdukt();
+
+    function visProdukt() {
+        // klone produkt_templaten
+        var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+
+        // indsæt data i klon
+
+
+
+        // append klon til .produktliste
+        document.querySelector(".produktliste").appendChild(klon);
+    }
+}
+
+
+
+
+
+
+
+
+
+//klon.querySelector(".data_billede").src = "imgd/small/" + produkt.billede + "-sm.jpg";
